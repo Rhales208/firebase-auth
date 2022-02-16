d84d4e122539c46dc3b7106521ba9def86302d6e
RuebenHales <halesrueben@gmail.com>
2022-02-16 10:34:13 -0500

    setup page and routing

---
+++ src/scenes/Login.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function login() {
+  return (
+    <>
+      <h1>Login</h1>
+      <p>
+        Not a user? <Link to="/signup"> </Link>{" "}
+      </p>
+    </>
+  );
+}
